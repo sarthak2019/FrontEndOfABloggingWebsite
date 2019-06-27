0f561d6121367a2970eaf4dfed90a71668f7dba2
codewarrior-X <raghavan_eiergm@yahoo.in>
2019-06-27 19:12:39 +0530

    New chnages with respect to close button and making fields *required

--- stub/js/header.js
+++ stub/js/header.js
@@ -1,13 +1,17 @@
 // Get the modal
 var modal = document.getElementById("myModal");
 var modal1 = document.getElementById("myModal1");
+var modal2 = document.getElementById("myModal2")
 
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn1");
 var btn1 = document.getElementById("myBtn2");
+var btn2 = document.getElementById("cpost");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
+var cls = document.getElementById("closepost");
+var clssignin = document.getElementById("csignin");
 
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
@@ -18,13 +22,26 @@ btn1.onclick = function() {
   modal1.style.display = "block";
 }
 
+btn2.onclick = function() {
+    modal2.style.display = "block";
+  }
+
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-modal.style.display = "none";
+    modal.style.display = "none";
+}
+
+cls.onclick = function(){
+    modal2.style.display = "none";
+}
+
+clssignin.onclick = function(){
+    modal1.style.display = "none";
 }
 
 
 // When the user clicks anywhere outside of the modal, close it
+
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
@@ -32,4 +49,5 @@ window.onclick = function(event) {
   else if (event.target == modal1) {
     modal1.style.display = "none";
   }
+
 }
