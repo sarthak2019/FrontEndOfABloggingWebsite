6d5d78024880716393b3d333b9a77859f667f1ec
Chetan RC <chetan4195@gmail.com>
2019-06-27 22:32:46 +0530

    added Function to update Like count

--- stub/js/post.js
+++ stub/js/post.js
@@ -10,3 +10,20 @@ function postEdit(){
                         saveState=0;
         }
 }
+
+
+var numberOfClicks=0;
+// To update like count
+function postLiked(){
+	document.getElementById("likeBlog").innerHTML = "Liked!";
+	numberOfClicks += 1;
+	if(numberOfClicks!=0){
+		if (numberOfClicks==1){
+		document.getElementById('likeCount').innerHTML = numberOfClicks + " person likes this!";
+		}
+		else {
+		document.getElementById('likeCount').innerHTML = numberOfClicks + " people have liked this!";
+		}
+	}
+}
+
