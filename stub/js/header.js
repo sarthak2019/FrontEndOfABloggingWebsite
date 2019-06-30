cb2a53154e16af43cc3b450b685135e3af7fb1fa
sarthak2019 <sarthakmohapatra2@gmail.com>
2019-06-30 20:49:43 +0530

    Corrected the header section and added CSS styles

--- stub/js/header.js
+++ stub/js/header.js
@@ -8,24 +8,37 @@ var clssignin = document.getElementsByClassName("close")[0];
 var clssignup = document.getElementsByClassName("close")[1];
 
 btn.onclick = function() {
+
   modal.style.display = "block";
+  
 }
 
 btn1.onclick = function() {
+
   modal1.style.display = "block";
+  
 }
 
 clssignup.onclick = function(){
+    document.getElementById('fullname').value = "";
+    document.getElementById('signupusername').value = "";
+    document.getElementById('signuppassword').value = "";
+    document.getElementById('confirmpassword').value = "";
     modal.style.display = "none";
+
 }
 
 clssignin.onclick = function(){
+
+    document.getElementById('signinusername').value = "";
+    document.getElementById('signinpassword').value = "";
     modal1.style.display = "none";
+
 }
 
 function signUp(){
-  
+
   modal1.style.display = "none";
   modal.style.display = "block";
-  
+
 }
