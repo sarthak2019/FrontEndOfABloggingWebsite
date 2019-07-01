5379df0adbbf1e1d2fa8480fff14c5e383d0adbb
sarthak2019 <sarthakmohapatra2@gmail.com>
2019-07-01 08:05:18 +0530

    Modified header.css and header.js files for the sign in and sign up modals

--- stub/js/header.js
+++ stub/js/header.js
@@ -1,44 +1,73 @@
+// Get the signup modal
 var modal = document.getElementById("myModal");
+
+// Get the signin modal
 var modal1 = document.getElementById("myModal1");
 
 var btn = document.getElementById("myBtn1");
 var btn1 = document.getElementById("myBtn2");
 
+// Get the <span> element that closes the modal
 var clssignin = document.getElementsByClassName("close")[0];
+
+// Get the <span> element that closes the modal1
 var clssignup = document.getElementsByClassName("close")[1];
 
-btn.onclick = function() {
+// When the user clicks the button, open the modal 
+btn.onclick = function () {
 
   modal.style.display = "block";
-  
-}
 
-btn1.onclick = function() {
+}
+// When the user clicks the button, open the modal 1
+btn1.onclick = function () {
 
   modal1.style.display = "block";
-  
+
 }
 
-clssignup.onclick = function(){
+// When the user clicks on <span> (x),  clear the text filed values if entered and then close the modal
+clssignup.onclick = function () {
+  document.getElementById('fullname').value = "";
+  document.getElementById('signupusername').value = "";
+  document.getElementById('signuppassword').value = "";
+  document.getElementById('confirmpassword').value = "";
+  modal.style.display = "none";
+
+}
+
+// When the user clicks on <span> (x),  clear the text filed values if entered and then close the modal
+clssignin.onclick = function () {
+
+  document.getElementById('signinusername').value = "";
+  document.getElementById('signinpassword').value = "";
+  modal1.style.display = "none";
+
+}
+
+//when user clicks on signup hyperlink on signin model, should open the signup model
+function signUp() {
+
+  document.getElementById('signinusername').value = "";
+  document.getElementById('signinpassword').value = "";
+  modal1.style.display = "none";
+  modal.style.display = "block";
+
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
     document.getElementById('fullname').value = "";
     document.getElementById('signupusername').value = "";
     document.getElementById('signuppassword').value = "";
     document.getElementById('confirmpassword').value = "";
     modal.style.display = "none";
-
-}
-
-clssignin.onclick = function(){
-
+  }
+  else if (event.target == modal1) {
     document.getElementById('signinusername').value = "";
     document.getElementById('signinpassword').value = "";
     modal1.style.display = "none";
-
-}
-
-function signUp(){
-
-  modal1.style.display = "none";
-  modal.style.display = "block";
+  }
 
 }
