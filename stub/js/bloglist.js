2ee4565b3820fe5a1b77d8e345b2d773d50d5df8
sarthak2019 <sarthakmohapatra2@gmail.com>
2019-07-01 07:49:55 +0530

    Added comments for bloglist.js file and made some changes for the sign in and sign up modals.

--- stub/js/bloglist.js
+++ stub/js/bloglist.js
@@ -1,3 +1,4 @@
+//When the window loads the below function will clip the conents of all the post description and will display the clipped content in the bloglist.html page.
 window.onload = function() {
     var postdescs = document.getElementsByClassName('post-desc');
 
@@ -8,14 +9,17 @@ window.onload = function() {
     }
 }
 
+//When the user clicks on the trash icon the delete modal will be visible.
 function trashModal(){
     document.getElementById('deleteModal').style.display = "block";
 }
 
+//When the user clicks on the No button present in the delete modal the delete modal will disappear and the bloglist page will appear again as it is.
 function noButton(){
     document.getElementById('deleteModal').style.display = "none";
 }
 
+//When the user clicks on ... icon then then it will redirect to a new page called as "post.html".
 function morePostDetails(){
     location.href = "post.html";
 }
